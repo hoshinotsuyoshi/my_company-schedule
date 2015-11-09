4fa56627a18d7c64623c59bc10ce4a51ab18cae4
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-11-09 11:04:53 +0900

    New year holidays

--- lib/my_company/schedule.rb
+++ lib/my_company/schedule.rb
@@ -2,10 +2,16 @@ require "increments/schedule"
 require "my_company/schedule/version"
 
 module MyCompany
-
-  HOLIDAYS = [
-    Date.new(2016, 7, 5) # Example.
-  ].freeze
+  HOLIDAYS = (2007..2106).map do |year|
+    [
+      Date.new(year, 1, 1),
+      Date.new(year, 1, 2),
+      Date.new(year, 1, 3),
+      Date.new(year, 1, 4),
+      Date.new(year, 12, 30),
+      Date.new(year, 12, 31),
+    ]
+  end.flatten.freeze
 
   module Schedule
     include Increments::Schedule
